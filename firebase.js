e0d4068e0cfb9f82168214ff1497b2c63e8f4810
redfyel <lianis.ee7@gmail.com>
2024-08-22 00:17:52 +0530

    changes in backend

--- firebase.js
+++ firebase.js
@@ -1,13 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 import {getFirestore} from "firebase/firestore";
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyDD-I1NlJLKShaZvhGlT49yzPXG2qqteI0",
   authDomain: "inventory-management-f0d06.firebaseapp.com",
@@ -20,7 +14,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const firestore = getFirestore(app)
 
 export {firestore}
